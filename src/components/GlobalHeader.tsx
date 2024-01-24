e2a15b3e51f36b1905d15dc25fc841133e2121f8
Nora Casey <1891757+norarcasey@users.noreply.github.com>
2024-01-23 23:30:09 -0800

    chore: missed a file

--- src/components/GlobalHeader.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export function GlobalHeader(): React.ReactElement {
-  return (
-    <header className="page-header">
-      <h1>Nora Casey</h1>
-    </header>
-  );
-}
